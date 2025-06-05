5335b6bd02fe18f2825ac463ed35392c5fa2c617
Ray-Auditore <rayyan0116@abudzar.sch.id>
2025-06-05 09:01:03 +0700

    selesai

--- vite-project/src/SAS.jsx
+++ vite-project/src/SAS.jsx
@@ -2,10 +2,9 @@ export default function SAS() {
   return (
     <div>
       <h1>SAS Rayyan Ghibran Ananta</h1>
-      <p>
-      SAS IT Rayyan
-      </p>
+      <p>SAS IT Rayyan</p>
       <img src="/public/images.jpg" alt="Logo SAS" />
     </div>
   );
 }
+
